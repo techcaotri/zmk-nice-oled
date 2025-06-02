ff9969d3fdd49cb9a3a8ee3934b96781f2265aee
Tri Pham <techcaotri@gmail.com>
2025-06-02 22:12:40 +0700

    Reorganize the modifier icons order to match the keymap: Shift, Control, Option/Alt, Win/Cmd

--- boards/shields/nice_oled/nice_epaper/widgets/modifiers_info.c
+++ boards/shields/nice_oled/nice_epaper/widgets/modifiers_info.c
@@ -65,7 +65,7 @@ struct modifier_symbol ms_cmd = {
 
 struct modifier_symbol *modifier_symbols[] = {
     // this order determines the order of the symbols
-    &ms_shift, &ms_opt, &ms_cmd, &ms_control};
+    &ms_shift, &ms_control, &ms_opt, &ms_cmd};
 
 #define NUM_SYMBOLS (sizeof(modifier_symbols) / sizeof(struct modifier_symbol *))
 
